59ce4587d77d683cfeb814956befd023b35283c1
Mustafo <mustaforahimov30@gmail.com>
2023-08-21 15:50:58 +0500

    End tic toc toe

--- app/src/main/java/uz/coder/tictoctoe/MainActivity.kt
+++ app/src/main/java/uz/coder/tictoctoe/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import uz.coder.tictoctoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +14,9 @@ class MainActivity : AppCompatActivity() {
     private var firstTurn = Turn.CROSS
     private var curretTurn = Turn.CROSS
     private var boardList = mutableListOf<Button>()
-    lateinit var binding: ActivityMainBinding
+    private var crossesScore = 0
+    private var noughtsScore = 0
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -34,9 +37,86 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun boardTapped(view: View) {
-        if (view !is Button)
+        if (view !is Button){
             return
+        }
         addToBoard(view)
+        if (fullBoard()){
+            result("Teng")
+        }else{
+            if (checkForVictory(NOUGHT)){
+                noughtsScore++
+                result("0 yutdi")
+            }else if (checkForVictory(CROSS)){
+                crossesScore++
+                result("X yutdi")
+            }
+        }
+    }
+
+    private fun checkForVictory(s: String): Boolean
+    {
+        //Horizantal wins
+        if(match(binding.a1,s)&&match(binding.a2,s)&&match(binding.a3,s)){
+            return true
+        }
+        if(match(binding.b1,s)&&match(binding.b2,s)&&match(binding.b3,s)){
+            return true
+        }
+        if(match(binding.c1,s)&&match(binding.c2,s)&&match(binding.c3,s)){
+            return true
+        }
+        //Vertical wins
+        if(match(binding.a1,s)&&match(binding.b1,s)&&match(binding.c1,s)){
+            return true
+        }
+        if(match(binding.a2,s)&&match(binding.b2,s)&&match(binding.c2,s)){
+            return true
+        }
+        if(match(binding.a3,s)&&match(binding.b3,s)&&match(binding.c3,s)){
+            return true
+        }
+        //Diagonal wins
+        if(match(binding.a1,s)&&match(binding.b2,s)&&match(binding.c3,s)){
+            return true
+        }
+        if(match(binding.a3,s)&&match(binding.b2,s)&&match(binding.c1,s)){
+            return true
+        }
+        return false
+    }
+    private fun match(button: Button,symbol:String):Boolean = button.text == symbol
+    private fun result(s: String) {
+        val massage  = "\n X : $crossesScore\n\n 0 : $noughtsScore"
+       AlertDialog.Builder(this@MainActivity)
+            .setTitle(s)
+            .setMessage(massage)
+            .setPositiveButton("Boshidan boshlash"){_,_->
+                resetBoard()
+            }
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun resetBoard() {
+        for (button in boardList){
+            button.text = ""
+        }
+        if (firstTurn == Turn.NOUGHT){
+            firstTurn = Turn.CROSS
+        }else if (firstTurn == Turn.CROSS){
+            firstTurn = Turn.NOUGHT
+        }
+        curretTurn = firstTurn
+        setTurnLabel()
+    }
+
+    private fun fullBoard(): Boolean {
+        for(button in boardList){
+            if (button.text == "")
+                return false
+        }
+        return true
     }
 
     private fun addToBoard(button: Button) {
@@ -56,9 +136,9 @@ class MainActivity : AppCompatActivity() {
     private fun setTurnLabel() {
         var turnText = ""
         if (curretTurn == Turn.CROSS)
-            turnText = "Turn $CROSS"
+            turnText = "Navbat: $CROSS"
         else if (curretTurn == Turn.NOUGHT)
-            turnText = "Turn $NOUGHT"
+            turnText = "Navbat: $NOUGHT"
         binding.turn.text = turnText
     }
 
